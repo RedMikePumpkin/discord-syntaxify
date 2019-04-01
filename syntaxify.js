b65d67955e640d88e48978d3db8389e186ccfea8
RedMikePumpkin <michael@malinov.com>
2019-04-01 16:57:19 -0700

    Update syntaxify.js

--- syntaxify.js
+++ syntaxify.js
@@ -1,1 +1,1 @@
-var Syntaxify=a=>{var b="",i=0;if(a===undefined)b="ERROR! a empty";else for(;i<a.length;i++)b+="||**~~__*`"+a[i]+"`*__~~**||";return b};
+var Syntaxify=a=>{var b="",i=0;if(!a&&a!=="")b="ERROR!";else for(;i<a.length;i++)b+="||**~~__*`"+a[i]+"`*__~~**||";return b};
